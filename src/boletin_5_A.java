4fd0490307ec41c48073abe2391cf453b72b82bf
sofiaotero-ascii <soterogil@danielcastelao.org>
2024-11-07 15:45:56 +0100

    Documentación

--- src/boletin_5_A.java
+++ src/boletin_5_A.java
@@ -1,3 +1,14 @@
+
+/**
+ * @version 2.0
+ * @autor Sofía Otero
+ * Utilizando Scanner y for creamos un progrma que pregunte en un bucle
+ * de 10 repeticiones por numeros enteros, los cuales se alamcenan luego en un
+ * String para después con un if, else if, else clasificar cuales de ellos
+ * son negativos o contienen un cero, mostrándolo por pantalla tras ser
+ * introducidos estos.Con los números que nos cumplan ninguna de estas condiciones
+ * no se mostrará información extra.
+ */
 import java.util.Scanner;//importamos un scanner para poder trabajar con datos externos
 public class boletin_5_A {
     public static void main(String[] args) {
@@ -14,10 +25,11 @@ public class boletin_5_A {
        }else if (numeros==0) {
            System.out.println("El numero es cero");
        }else{//Si es positivo
-           if(numeros_para_casos.contains("0"){
-               System.out.println("El numero tiene 0"+numeros);
+           if(numeros_para_casos.contains("0")){
+               System.out.println("Tu numero "+numeros+" contiene 0");
            }
 
        }
        }
     }
+}
